1518678b1f6be757a6acb928377b1bd91f106ceb
MotorTruck1221 <tuckerj0606@icloud.com>
2022-12-19 00:33:25 -0700

    Add Metadata crap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,7 +21,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       // og tags
       <meta property="og:title" content="Natant Games" />
       <meta property="og:description" content="Natant Games is a website with a relatively clean ui" />
-      <meta property="og:image" content="https://natantgames.com/assets/LOGO.svg" />
+      <meta property="og:image" content="https://unblockedhaven.games/assets/LOGO.svg" />
       <meta property="og:url" content="https://unblockedhaven.games" />
       <meta property="og:site_name" content="Natant Games" />
       <meta property="og:type" content="website" />
