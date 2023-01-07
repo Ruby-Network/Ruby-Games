652497f46ff84c1ba651e6ff1f4676ef2901e439
MotorTruck1221 <tuckerj0606@icloud.com>
2023-01-07 12:51:13 -0700

    Ruby

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,19 +12,19 @@ function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
   return (
     <>
-      <title>Natant Games</title>
+      <title>Ruby</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="description" content="Natant Games is a website with a relatively clean ui" />
-      <meta name="keywords" content="Natant Network, Natant Games" />
-      <meta name="author" content="Natant Network" />
-      <meta name="application-name" content="Natant Games" />
+      <meta name="description" content="Ruby is a website with a relatively clean ui" />
+      <meta name="keywords" content="Ruby Network" />
+      <meta name="author" content="Ruby Network" />
+      <meta name="application-name" content="Ruby" />
       <script src="https://cpanel.motortruck1221.tech/no.js" defer></script>
       // og tags
-      <meta property="og:title" content="Natant Games" />
-      <meta property="og:description" content="Natant Games is a website with a relatively clean ui" />
+      <meta property="og:title" content="Ruby" />
+      <meta property="og:description" content="Ruby is a website with a relatively clean ui" />
       <meta property="og:image" content="https://unblockedhaven.games/assets/LOGO.svg" />
       <meta property="og:url" content="https://unblockedhaven.games" />
-      <meta property="og:site_name" content="Natant Games" />
+      <meta property="og:site_name" content="Ruby" />
       <meta property="og:type" content="website" />
       <meta property="og:locale" content="en_US" />
       <link id="faviconico" rel="icon" href="/favicon.png" />
